c4c95a4dd628feb1dc42d99ec3d538e927a6c71a
José Tomás González <jtgonzalez@miuandes.cl>
2017-04-07 09:08:12 -0300

    Added required course model validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,6 +2,10 @@ class Course < ApplicationRecord
   has_many :enrollments
   has_many :students, through: :enrollments
   belongs_to :teacher, class_name: 'Person', foreign_key: 'person_id'
+  
+  validates :title, presence: true, length: {maximum: 30}, uniqueness: true
+  validates :code, presence: true, length: {maximum: 10}, uniqueness: true
+  validates :quota, presence: true, numericality: { only_integer: true }
 
   def to_s
     return title
