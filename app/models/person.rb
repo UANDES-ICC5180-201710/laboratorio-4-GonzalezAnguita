8f87cf611d36f64ff9db07aaa225654c8f78d1e7
José Tomás González <jtgonzalez@miuandes.cl>
2017-04-07 08:56:57 -0300

    Added required person model validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,14 @@
 class Person < ApplicationRecord
   has_many :enrollements, through: :enrollments
+  
+  validates :first_name, presence: true, length: {maximum: 30}
+  validates :last_name, presence: true, length: {maximum: 30}
+  validates :email, presence: true,
+            length: {maximum: 50},
+            uniqueness: true,
+            confirmation: true,
+            format: { with: /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i}
+  validates :email_confirmation, presence: true
 
   def to_s
     return first_name + ' ' + last_name
